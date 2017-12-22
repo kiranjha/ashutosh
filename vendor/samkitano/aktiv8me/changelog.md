f474bcdbc9e42cb02feaa54eece8f8cf51317380
Ashutosh <ashutosh.sharma366@gmail.com>
2017-12-22 11:14:27 +0530

    vendor folder
    Signed-off-by: Ashutosh <ashutosh.sharma366@gmail.com>

---
+++ vendor/samkitano/aktiv8me/changelog.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## [1.1.0] - 30-07-2017
+
+### Fixed major bug in Login Stub
+Stub was missing the check for the active property on the authenticated user.
+
+## [2.0.0] - 06-09-2017
+
+### Added support for Laravel 5.5
---
+++ vendor/samkitano/aktiv8me/tests/Feature/EmailsTestDisabled.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+//use Carbon\Carbon;
+//use Tests\TestCase;
+//use Kitano\Aktiv8me\ActivatesUsers;
+//use MailThief\Testing\InteractsWithMail;
+//use Illuminate\Foundation\Testing\DatabaseMigrations;
+//
+//class EmailsTest extends TestCase
+//{
+//    use ActivatesUsers, DatabaseMigrations, InteractsWithMail;
+//
+//    protected $user;
+//
+//    public function setUp()
+//    {
+//        parent::setUp();
+//
+//        $user = factory(\App\User::class)->create();
+//
+//        $this->user = $user;
+//    }
+
+//    public function testSendActivationEmail()
+//    {
+//        $this->sendActivationEmail($this->user, \App\RegistrationToken::makeToken($this->user->email));
+//
+//        $this->seeMessageFor($this->user->email);
+//        $this->seeMessageWithSubject(trans('aktiv8me.notifications.confirm.subject'));
+//        $this->assertTrue($this->lastMessage()->contains($this->user->codes->first()->token));
+//    }
+//
+//    public function testSendUserIsActiveEmail()
+//    {
+//        $this->sendUserIsActiveEmail($this->user);
+//
+//        $this->seeMessageFor($this->user->email);
+//        $this->seeMessageWithSubject(trans('aktiv8me.notifications.isactive.subject'));
+//        $this->assertTrue($this->lastMessage()->contains(config('app.name')));
+//        $this->assertTrue($this->lastMessage()->contains(route('password.request')));
+//    }
+//
+//    public function testSendWelcomeEmail()
+//    {
+//        config(['aktiv8me.welcome_email' => true]);
+//
+//        $this->sendWelcomeEmail($this->user);
+//
+//        $this->seeMessageFor($this->user->email);
+//        $this->seeMessageWithSubject(trans('aktiv8me.notifications.welcome.subject'));
+//    }
+//
+//    public function testSendTokenUpdatedEmail()
+//    {
+//        $token = \App\RegistrationToken::createFor($this->user);
+//        $updated_token = \App\RegistrationToken::updateFor($this->user);
+//
+//        $this->sendTokenUpdatedEmail($this->user, $updated_token);
+//
+//        $this->seeMessageFor($this->user->email);
+//        $this->seeMessageWithSubject(trans('aktiv8me.notifications.confirm.subject'));
+//        $this->assertTrue($this->lastMessage()->contains($updated_token->token));
+//        $this->assertFalse($this->lastMessage()->contains($token->token));
+//    }
+//}
